f1dbf624825d5d0c39c7d382607baa94d9552450
Hansraj Deghun <hansrajdeghun@gofynd.com>
2025-01-05 15:59:52 +0530

    implemented thread safety for memory  handling by appling mutexes

--- models/key_dir.go
+++ models/key_dir.go
@@ -14,6 +14,7 @@ type KeyDir struct {
 
 var once sync.Once
 var InMemoryStorage map[string]KeyDir
+var mu sync.Mutex // Mutex for synchronizing memory writes
 
 // init initializes InMemoryStorage
 func Init() {
@@ -24,19 +25,25 @@ func Init() {
 }
 
 func AddToMemory(FileName, Key string, RecordSize int, RecordPosition int64, Timestamp int64) {
+	mu.Lock()
+
 	_KeyDir := KeyDir{
 		RecordSize:     RecordSize,
 		RecordPosition: RecordPosition,
 		FileName:       FileName,
 		Timestamp:      Timestamp,
 	}
-	fmt.Print("Adding to memory: ", _KeyDir, "\n")
 	InMemoryStorage[Key] = _KeyDir
+	defer mu.Unlock()
 	fmt.Print("Added to memory: ", InMemoryStorage[FileName], "\n")
+
 }
 func GetDataFromMemory(key string) (KeyDir, bool) {
+	mu.Lock()
+
 	value, exists := InMemoryStorage[key]
 	fmt.Print("Getting from memory: ", value, "\n")
+	defer mu.Unlock()
 	if !exists {
 		return value, exists
 	}
